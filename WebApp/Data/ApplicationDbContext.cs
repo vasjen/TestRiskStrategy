50ef2b21d5e5ae1e56b6751155122291cf39dcfd
Vasjen <vasjenm@gmail.com>
2023-07-21 18:53:31 +0400

    Initial commit

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration config)
+        : base(options)
+    {
+    }
+
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("Table");
+        
+    }
+
+    protected override void OnModelCreating (ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+    }
+}
